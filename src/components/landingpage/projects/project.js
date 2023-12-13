94735498d47e10c59ad86d46702d31c308869350
mugunthancloudin <mugunthan@cloudinlabs.com>
2023-12-13 15:22:05 +0530

    responsive fixed

--- src/components/landingpage/projects/project.js
+++ src/components/landingpage/projects/project.js
@@ -10,7 +10,9 @@ export default function Project() {
   return (
     <>
       <div className="container-fluid projectsBg pt-5 pb-5" id="action3">
-        <div className="row"><h1 className="text-center projectTitleH1 fw-bold ">Projects</h1></div>
+        <div className="row">
+          <h1 className="text-center projectTitleH1 fw-bold ">Projects</h1>
+        </div>
         <div className="container">
           <div className="row">
             <div className="col-lg-6  mt-5 d-flex align-items-start">
@@ -19,18 +21,7 @@ export default function Project() {
               </video>
             </div>
             <div className="col-lg-6">
-              <h1 className="projectH1 d-flex text-end">
-                {/* <spam className="arrSpam">
-                  <img
-                    src={left}
-                    alt="left-signol"
-                    className="projectLeftImage"
-                  />{" "}
-                </spam> */}
-                <spam className="projectTitleContent text-wrap">
-                  Bull's-Call
-                </spam>
-              </h1>
+              <h1 className="projectH1 text-center">Bull's-Call</h1>
               <p className="fw-bold fst-italic mt-3">
                 Bulls Call is a betting game that is now available on the
                 Polygon Mumbai Testnet and is based on cryptocurrency candle
@@ -38,39 +29,27 @@ export default function Project() {
                 receives 5% of each wager in fees. The payout is the amount you
                 will be paid out after you place a wager.
               </p>
-              <button className="bullsCall-button">
-                P L A Y
-                <div className="bullsCall-clip">
-                  <div className="bullsCall-corner bullsCall-left-top"></div>
-                  <div className="bullsCall-corner bullsCall-right-bottom"></div>
-                  <div className="bullsCall-corner bullsCall-right-top"></div>
-                  <div className="bullsCall-corner bullsCall-left-bottom"></div>
-                </div>
-                <span className="bullsCall-arrow bullsCall-right-arrow"></span>
-                <span className="bullsCall-arrow bullsCall-left-arrow"></span>
-              </button>
+              <div className="text-center">
+                <button className="bullsCall-button">
+                  P L A Y
+                  <div className="bullsCall-clip">
+                    <div className="bullsCall-corner bullsCall-left-top"></div>
+                    <div className="bullsCall-corner bullsCall-right-bottom"></div>
+                    <div className="bullsCall-corner bullsCall-right-top"></div>
+                    <div className="bullsCall-corner bullsCall-left-bottom"></div>
+                  </div>
+                  <span className="bullsCall-arrow bullsCall-right-arrow"></span>
+                  <span className="bullsCall-arrow bullsCall-left-arrow"></span>
+                </button>
+              </div>
             </div>
           </div>
-
-  
         </div>
 
         <div className="container" id="action4">
-          <div className="row">
-            <div className="col-lg-6">
-              <h1 className="projectH1 d-flex text-end">
-                
-                <spam className="MediChainTitleContent text-wrap">
-                  MediChain
-                </spam>
-                {/* <spam className="arrMediChainSpam">
-                  <img
-                    src={right}
-                    alt="left-signol"
-                    className="projectLeftImage"
-                  />{" "}
-                </spam> */}
-              </h1>
+          <div className="row ">
+            <div className="col-lg-6 order-lg-1 order-sm-2">
+              <h1 className="projectH1  text-center">MediChain</h1>
               <p className="fw-bold fst-italic mt-3">
                 Bulls Call is a betting game that is now available on the
                 Polygon Mumbai Testnet and is based on cryptocurrency candle
@@ -78,26 +57,26 @@ export default function Project() {
                 receives 5% of each wager in fees. The payout is the amount you
                 will be paid out after you place a wager.
               </p>
-              <div class="container-button mt-5 ms-5">
-                <div className="container-button">
-                  <div className="custom-hover bt-1"></div>
-                  <div className="custom-hover bt-2"></div>
-                  <div className="custom-hover bt-3"></div>
-                  <div className="custom-hover bt-4"></div>
-                  <div className="custom-hover bt-5"></div>
-                  <div className="custom-hover bt-6"></div>
-                  <button className="custom-button"></button>
+              <div className="text-center justify-content-center align-items-center d-flex">
+                <div class="container-button mt-5">
+                  <div className="container-button">
+                    <div className="custom-hover bt-1"></div>
+                    <div className="custom-hover bt-2"></div>
+                    <div className="custom-hover bt-3"></div>
+                    <div className="custom-hover bt-4"></div>
+                    <div className="custom-hover bt-5"></div>
+                    <div className="custom-hover bt-6"></div>
+                    <button className="custom-button"></button>
+                  </div>
                 </div>
               </div>
             </div>
-            <div className="col-lg-6  mt-5 d-flex align-items-start">
+            <div className="col-lg-6 mt-5 d-flex align-items-start order-lg-2 order-sm-1">
               <video width="100%" loop muted controls className="rounded-3">
                 <source src={MediChainvideoSource} type="video/mp4" />
               </video>
             </div>
           </div>
-
-        
         </div>
 
         <div className="container" id="action5">
@@ -108,18 +87,7 @@ export default function Project() {
               </video>
             </div>
             <div className="col-lg-6">
-              <h1 className="projectH1 d-flex text-end">
-                {/* <spam className="arrSpam">
-                  <img
-                    src={left}
-                    alt="left-signol"
-                    className="projectLeftImage"
-                  />{" "}
-                </spam> */}
-                <spam className="projectTitleContent text-wrap">
-                  Doc-Verify
-                </spam>
-              </h1>
+              <h1 className="projectH1 text-center">Doc-Verify</h1>
               <p className="fw-bold fst-italic mt-3">
                 Bulls Call is a betting game that is now available on the
                 Polygon Mumbai Testnet and is based on cryptocurrency candle
@@ -127,13 +95,13 @@ export default function Project() {
                 receives 5% of each wager in fees. The payout is the amount you
                 will be paid out after you place a wager.
               </p>
-              <button class="doc-button">Verify Now</button>
+              <div className="text-center">
+                <button className="doc-button">Verify Now</button>
+              </div>
             </div>
           </div>
-
         </div>
       </div>
     </>
   );
 }
- 
--- src/components/landingpage/workflow/process.js
+++ src/components/landingpage/workflow/process.js
@@ -22,13 +22,13 @@ export default function Process() {
       <div className="container-fluid pt-5 pb-5" id="action6">
         <div className="container">
           <div className="row">
-            <h1 className="fw-bold processH1 text-center mb-4">Process We Follow</h1>
+            <h1 className="fw-bold processH1 text-center mb-2">Process We Follow</h1>
           </div>
           
           {/* Use col-lg-3 layout on larger screens */}
           <div className="row d-none d-lg-flex">
             {process.map((card) => (
-              <div key={card.id} className="col-lg-3 mb-4">
+              <div key={card.id} className="col-lg-3 mt-4 mb-4" style={{ margin: "0 auto" }}>
                 <div className="card shadow d-flex flex-column justify-content-around">
                   {/* Your card content */}
                   <img
